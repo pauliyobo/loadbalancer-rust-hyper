ad6edf12080f9253a9eafac2d07ba811e0814729
pauliyobo <pauliyobo@gmail.com>
2024-03-13 19:08:07 +0100

    feat!: Migrated to hyper 1.0

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,86 @@
 use anyhow::Result;
-use hyper::server::conn::AddrStream;
-use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Client, Request, Response, Uri};
-use tokio::time::timeout;
+use http_body::Body;
+use http_body_util::combinators::BoxBody;
+use http_body_util::{BodyExt, Full};
+use hyper::body::Bytes;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{body, Request, Response, Uri};
+use hyper_util::client::legacy::Client;
+use hyper_util::rt::TokioExecutor;
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
+
+// use tokio::time::timeout;
+use std::marker::PhantomData;
 use std::net::SocketAddr;
 use std::str::FromStr;
 use std::sync::{
     atomic::{AtomicU64, Ordering},
     Arc, Mutex,
 };
-use std::time::{Instant, Duration};
-use tracing::{info, error};
+use std::time::Instant;
+use tracing::{debug, error, info};
 
 /// A server to which the load balancer will route requests
 #[derive(Clone, Debug)]
-struct Server {
+struct Server<B> {
     /// Server port
     pub port: u16,
     /// Server address
     pub addr: String,
+    phantom: PhantomData<B>,
 }
 
-impl Server {
+impl<B> Server<B>
+where
+    B: Body + Send + 'static + Unpin,
+    B::Data: Send,
+    B::Error: Into<Box<dyn std::error::Error + Send + Sync>>,
+{
     pub fn new(addr: &str, port: u16) -> Self {
         Self {
             addr: addr.into(),
             port,
+            phantom: PhantomData,
         }
     }
 
     /// Forward an incoming request from the load balancer
-    pub async fn forward(&self, req: Request<Body>) -> Result<Response<Body>> {
-        let path = req.uri().path().to_owned();
-        let uri = format!("http://{}:{}{}", self.addr, self.port, path);
+    pub async fn forward(
+        &self,
+        req: Request<body::Incoming>,
+    ) -> Result<Response<BoxBody<Bytes, hyper::Error>>> {
+        let uri = req.uri().to_string();
+        debug!("Uri received from request, {}", uri);
+        let uri = format!("http://{}:{}{}", self.addr, self.port, uri);
         info!("Forwarding request to {}", uri);
         let mut request = req;
         *request.uri_mut() = Uri::from_str(&uri)?;
         let now = Instant::now();
-        let response = Client::new().request(request).await?;
-        info!("{:?}", response);
-        info!("Received response in {}", humantime::format_duration(now.elapsed()));
-        Ok(response)
+        let response = Client::builder(TokioExecutor::new())
+            .build_http()
+            .request(request)
+            .await;
+        debug!("Received response, {:?}", response);
+        info!(
+            "Received response in {}",
+            humantime::format_duration(now.elapsed())
+        );
+        Ok(Response::new(
+            response
+                .map_err(|e| {
+                    error!("{}", e);
+                    e
+                })?
+                .boxed(),
+        ))
     }
 }
 
 #[derive(Clone, Debug)]
 struct Balancer {
-    pub servers: Vec<Server>,
+    pub servers: Vec<Server<Full<Bytes>>>,
     next_request_index: Arc<Mutex<AtomicU64>>,
 }
 
@@ -75,7 +109,10 @@ impl Balancer {
     }
 
     /// distribute an incoming request
-    pub async fn distribute(&self, req: Request<Body>) -> Result<Response<Body>> {
+    pub async fn distribute(
+        &self,
+        req: Request<body::Incoming>,
+    ) -> Result<Response<BoxBody<Bytes, hyper::Error>>> {
         // get the index of the server that will receive the request
         let index = self.next_id();
         info!("Forwarding request to server at index {}", index);
@@ -84,7 +121,7 @@ impl Balancer {
 }
 
 /// checks periodically which servers are healthy
-async fn heartbeat(balancer: Arc<Balancer>) -> Result<()> {
+/* async fn heartbeat(balancer: Arc<Balancer>) -> Result<()> {
     for srv in balancer.servers.iter() {
         let uri: Uri = format!("http://{}:{}/", srv.addr, srv.port).parse()?;
         let _ = tokio::spawn(async {
@@ -110,42 +147,39 @@ async fn heartbeat(balancer: Arc<Balancer>) -> Result<()> {
         });
     }
     Ok(())
-}
-
-async fn handler(balancer: Arc<Balancer>, req: Request<Body>) -> Result<Response<Body>> {
-    let res = balancer.distribute(req);
-    Ok(res.await?)
-}
+}*/
 
 #[tokio::main]
 async fn main() {
     // set up logging
     tracing_subscriber::fmt::init();
     // Create the load balancer
-    let state = Arc::new(Balancer::new().with_server(&"127.0.0.1:8001".parse().unwrap()).with_server(&"127.0.0.1:8000".parse().unwrap()));
-    let state_clone = state.clone();
+    let state = Arc::new(
+        Balancer::new()
+            .with_server(&"127.0.0.1:8001".parse().unwrap())
+            .with_server(&"127.0.0.1:8000".parse().unwrap()),
+    );
     let addr: SocketAddr = "0.0.0.0:80".parse().unwrap();
-    let sv = make_service_fn(move |con: &AddrStream| {
-        info!("Incoming connection from {}", con.remote_addr());
-        let state = state.clone();
-        async move {
-            anyhow::Ok(service_fn(move |req: Request<Body>| {
-                handler(state.clone(), req)
-            }))
-        }
-    });
+    let listener = TcpListener::bind(addr).await.unwrap();
 
-    let server = hyper::Server::bind(&addr).serve(sv);
-    let _ = tokio::spawn(async move {
-        loop {
+    loop {
+        let state_clone = state.clone();
+        let (stream, _) = listener.accept().await.unwrap();
+        let io = TokioIo::new(stream);
+        tokio::task::spawn(async move {
             let state = state_clone.clone();
-            info!("Running health check.");
-            heartbeat(state)        .await.unwrap();
-            tokio::time::sleep(Duration::from_secs(60)).await;
-        }
-    });
-
-    if let Err(e) = server.await {
-        error!("{}", e);
+            if let Err(e) = http1::Builder::new()
+                .serve_connection(
+                    io,
+                    service_fn(|req: Request<body::Incoming>| {
+                        let state = state.clone();
+                        async move { state.distribute(req).await }
+                    }),
+                )
+                .await
+            {
+                error!("Error while serving connection, {}", e);
+            }
+        });
     }
 }
